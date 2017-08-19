32930a38a0e895a0ea33994f62a60709356280a3
Lani Asep Sutisna <lani.asep@lawencon.com>
2017-08-19 18:51:55 +0700

    EM Crud Base of Foundation

---
+++ page_guru.php
@@ -0,0 +1,148 @@
+<?php include "pages/header.php"; ?>
+  <h4>DATA GURU</h4>
+  <p>Ini adalah data guru keseluruhan English Muslim yang menjadi bagian penting dari English Muslim. Didalam data guru kamu bisa melakukan beberapa management terkait data guru. Contohnya kamu bisa melakukan <code>Tambah</code>, <code>Edit</code>, <code>Hapus</code> dan <code>Laporan</code> guru yang bisa diproses.</p>
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Nama</th>
+          <th>Tanggal Lahir</th>
+          <th>Mata Pelajaran</th>
+          <th>Jml Jam / Minggu</th>
+          <th>Jadwal</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1,001</td>
+          <td>Lorem</td>
+          <td>ipsum</td>
+          <td>dolor</td>
+          <td>sit</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,002</td>
+          <td>amet</td>
+          <td>consectetur</td>
+          <td>adipiscing</td>
+          <td>elit</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,003</td>
+          <td>Integer</td>
+          <td>nec</td>
+          <td>odio</td>
+          <td>Praesent</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,003</td>
+          <td>libero</td>
+          <td>Sed</td>
+          <td>cursus</td>
+          <td>ante</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,004</td>
+          <td>dapibus</td>
+          <td>diam</td>
+          <td>Sed</td>
+          <td>nisi</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,005</td>
+          <td>Nulla</td>
+          <td>quis</td>
+          <td>sem</td>
+          <td>at</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,006</td>
+          <td>nibh</td>
+          <td>elementum</td>
+          <td>imperdiet</td>
+          <td>Duis</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,007</td>
+          <td>sagittis</td>
+          <td>ipsum</td>
+          <td>Praesent</td>
+          <td>mauris</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,008</td>
+          <td>Fusce</td>
+          <td>nec</td>
+          <td>tellus</td>
+          <td>sed</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,009</td>
+          <td>augue</td>
+          <td>semper</td>
+          <td>porta</td>
+          <td>Mauris</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,010</td>
+          <td>massa</td>
+          <td>Vestibulum</td>
+          <td>lacinia</td>
+          <td>arcu</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,011</td>
+          <td>eget</td>
+          <td>nulla</td>
+          <td>Class</td>
+          <td>aptent</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,012</td>
+          <td>taciti</td>
+          <td>sociosqu</td>
+          <td>ad</td>
+          <td>litora</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,013</td>
+          <td>torquent</td>
+          <td>per</td>
+          <td>conubia</td>
+          <td>nostra</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,014</td>
+          <td>per</td>
+          <td>inceptos</td>
+          <td>himenaeos</td>
+          <td>Curabitur</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+        <tr>
+          <td>1,015</td>
+          <td>sodales</td>
+          <td>ligula</td>
+          <td>in</td>
+          <td>libero</td>
+          <td>10:00 - 11:00 WIB</td>
+        </tr>
+      </tbody>
+    </table>
+
+<?php include "pages/footer.php"; ?>
---
+++ pages/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://dev.englishmuslim.com/images/em.png">
+
+    <title>Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/navbar.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="assets/images/logo.png" class="d-inline-block align-top" alt=""> <b>ENGLISH MUSLIM</b>
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample09">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link lead-nav" href="index.php">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle lead-nav" href="http://example.com" id="dropdown09" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><b>Menu</b></a>
+            <div class="dropdown-menu" aria-labelledby="dropdown09">
+              <a class="dropdown-item lead-nav" href="page_guru.php">Guru</a>
+              <a class="dropdown-item lead-nav" href="#">Siswa</a>
+              <a class="dropdown-item lead-nav" href="#">Data Karyawan</a>
+              <a class="dropdown-item lead-nav" href="#">Data Customer</a>
+              <a class="dropdown-item lead-nav" href="#">Peserta Seminar</a>
+              <a class="dropdown-item lead-nav" href="#">Data List Client EM</a>
+              <a class="dropdown-item lead-nav" href="#">Data Keungan EM</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link lead-nav" href="#">Kontak EM</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link lead-nav" href="#">Tentang EM</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Cari" aria-label="Cari">
+        </form>
+      </div>
+    </nav>
+
+  <div class="container">
